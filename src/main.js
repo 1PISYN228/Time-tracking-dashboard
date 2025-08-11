16c18b6005f5021e6c2dad139b0e6ad5b7c98bb8
1PISYN228 <kastromatvuy@gmail.com>
2025-08-11 17:40:05 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -62,7 +62,7 @@ let activityData = []
 
 
 
-fetch("../data.json").then((response) => {
+fetch("http://localhost:5500/data.json").then((response) => {
     if (!response.ok) {
         const messageError = "Something went wrong!"
         throw new Error(messageError)
